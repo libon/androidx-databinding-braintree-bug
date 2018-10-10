1ea7ba58ed65d1a104d7b3c07752c8d4e0788163
Carmen Alvarez <calvarez@libon.com>
2018-10-10 17:06:49 +0200

    Add 2-way databinding and log the input as it's typed.

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -1,16 +1,35 @@
 package com.example.myapplication;
 
 import android.databinding.DataBindingUtil;
+import android.databinding.Observable;
+import android.databinding.ObservableField;
+import android.databinding.ViewDataBinding;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 
 import com.example.myapplication.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
 
+    private static final String TAG = MainActivity.class.getSimpleName();
+
+    // Should be a viewmodel but just going for simplicity to reproduce the bug
+    public static class Model {
+        public ObservableField<CharSequence> inputText = new ObservableField<>();
+    }
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         ActivityMainBinding binding = DataBindingUtil.setContentView(this, R.layout.activity_main);
+        final Model model = new Model();
+
+        binding.setModel(model);
+        model.inputText.addOnPropertyChangedCallback(new Observable.OnPropertyChangedCallback() {
+            @Override
+            public void onPropertyChanged(Observable sender, int propertyId) {
+                Log.v(TAG, "inputText changed: " + model.inputText.get());
+            }
+        });
     }
 }
