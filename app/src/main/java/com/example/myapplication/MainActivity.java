3fc4a6feec17ee57f5707937e5d0f2c6483fe4fb
quinnjn <quinn@neumiiller.com>
2018-10-11 14:35:10 -0700

    Exclude visa-checkout from braintree-android
    Visa Checkout uses databinding and seems to be a blocker for the
    AndroidX upgrade. Excluding the package from braintree.
    
    This will prevent merchants from processing Visa Checkout until it is
    fixed but this is a temporary fix.
    
    Also including a test Card tokenization to verify that Braintree's
    tokenization flow works with AndroidX.

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -1,18 +1,31 @@
 package com.example.myapplication;
 
+import android.os.Bundle;
+import android.util.Log;
+import android.widget.Toast;
+
+import com.braintreepayments.api.BraintreeFragment;
+import com.braintreepayments.api.Card;
+import com.braintreepayments.api.exceptions.InvalidArgumentException;
+import com.braintreepayments.api.interfaces.BraintreeErrorListener;
+import com.braintreepayments.api.interfaces.PaymentMethodNonceCreatedListener;
+import com.braintreepayments.api.models.CardBuilder;
+import com.braintreepayments.api.models.PaymentMethodNonce;
+import com.example.myapplication.databinding.ActivityMainBinding;
+
+import androidx.appcompat.app.AppCompatActivity;
 import androidx.databinding.DataBindingUtil;
 import androidx.databinding.Observable;
 import androidx.databinding.ObservableField;
-import androidx.databinding.ViewDataBinding;
-import androidx.appcompat.app.AppCompatActivity;
-import android.os.Bundle;
-import android.util.Log;
 
-import com.example.myapplication.databinding.ActivityMainBinding;
-
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements PaymentMethodNonceCreatedListener,
+        BraintreeErrorListener {
 
     private static final String TAG = MainActivity.class.getSimpleName();
+
+    // Stolen from: https://github.com/braintree/braintree_android/blob/master/Demo/src/main/java/com/braintreepayments/demo/Settings.java#L19
+    private static final String SANDBOX_TOKENIZATION_KEY = "sandbox_tmxhyf7d_dcpspy2brwdjr3qn";
+    private BraintreeFragment mBraintreeFragment;
 
     // Should be a viewmodel but just going for simplicity to reproduce the bug
     public static class Model {
@@ -31,5 +44,26 @@ public class MainActivity extends AppCompatActivity {
                 Log.v(TAG, "inputText changed: " + model.inputText.get());
             }
         });
+
+        try {
+            mBraintreeFragment = BraintreeFragment.newInstance(this,
+                    SANDBOX_TOKENIZATION_KEY);
+        } catch (InvalidArgumentException e) {
+            onError(e);
+        }
+
+        Card.tokenize(mBraintreeFragment, new CardBuilder()
+                        .cardNumber("4111111111111111"));
+    }
+
+    @Override
+    public void onPaymentMethodNonceCreated(PaymentMethodNonce paymentMethodNonce) {
+        Toast.makeText(this, "Success " + paymentMethodNonce.getNonce(),
+                Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    public void onError(Exception error) {
+        throw new RuntimeException(error);
     }
 }
